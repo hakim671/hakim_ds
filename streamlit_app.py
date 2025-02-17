8e966fbad0eb75f552ab894a50344b79a584fc37
hakim671 <boyzodahakim2008@gmail.com>
2025-02-17 19:49:08 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,13 @@ import streamlit as st
 st.title('🎈 App Hakim')
 
 st.write('Hello world!')
+
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+with st.expander('data'):
+  st.write("X")
+  X_row = df.drop("species", axis=1)
+  st.dataframe(X_row)
+  st.write("y")
+  y_row = df.species
+  st.dataframe(y_row)
